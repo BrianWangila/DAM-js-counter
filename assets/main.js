b607bb531f167c2ee785660e76effc99b4adc88f
Brian Wangila <bwanjalah@gmail.com>
2022-06-23 22:19:14 +0300

    assignment complete

--- assets/main.js
+++ assets/main.js
@@ -5,4 +5,26 @@ const disp = document.querySelector("#display-heading")
 button5.addEventListener('click', () => {
   disp.textContent = "added 5"
   indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
+
+
 })
+
+const button10 = document.getElementById("plusTen")
+const button15 = document.getElementById("plusFifteen")
+const button20 = document.getElementById("plusTwenty")
+
+function addingButtons () {
+  button10.addEventListener("click", () => {
+    indicator.textContent = (parseInt(indicator.textContent) + 10)
+  })
+
+  button15.addEventListener("click", () => {
+    indicator.textContent = (parseInt(indicator.textContent) + 15)
+  })
+
+  button20.addEventListener("click", () => {
+    indicator.textContent = (parseInt(indicator.textContent) + 20)
+  })
+
+}
+addingButtons()
